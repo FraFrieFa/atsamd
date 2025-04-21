70323f093ac78e476b3efe39119e938bb8db6b39
unguentum <witherbones1@gmail.com>
2025-04-21 17:10:30 +0200

    Common Pads struct

--- hal/src/sercom/pad.rs
+++ hal/src/sercom/pad.rs
@@ -261,6 +261,55 @@ where
 }
 
 //==============================================================================
+// Pads
+//==============================================================================
+
+struct Pads<P0: OptionalPad, P1: OptionalPad, P2: OptionalPad, P3: OptionalPad>(P0, P1, P2, P3);
+
+trait IsPads {}
+impl<P0: OptionalPad, P1: OptionalPad, P2: OptionalPad, P3: OptionalPad> IsPads
+    for Pads<P0, P1, P2, P3>
+{
+}
+
+trait ReplacePad<P: PadNum> : IsPads
+{
+    fn replace<I: IsPad<PadNum = P>>(self, pin: I) -> impl IsPads;
+}
+
+impl<P1: OptionalPad, P2: OptionalPad, P3: OptionalPad> ReplacePad<Pad0>
+    for Pads<NoneT, P1, P2, P3>
+{
+    fn replace<I: IsPad<PadNum = Pad0>>(self, pin: I) -> impl IsPads {
+        Pads(pin, self.1, self.2, self.3)
+    }
+}
+
+impl<P0: OptionalPad, P2: OptionalPad, P3: OptionalPad> ReplacePad<Pad1>
+    for Pads<P0, NoneT, P2, P3>
+{
+    fn replace<I: IsPad<PadNum = Pad1>>(self, pin: I) -> impl IsPads {
+        Pads(self.0, pin, self.2, self.3)
+    }
+}
+
+impl<P0: OptionalPad, P1: OptionalPad, P3: OptionalPad> ReplacePad<Pad2>
+    for Pads<P0, P1, NoneT, P3>
+{
+    fn replace<I: IsPad<PadNum = Pad2>>(self, pin: I) -> impl IsPads {
+        Pads(self.0, self.1, pin, self.3)
+    }
+}
+
+impl<P0: OptionalPad, P1: OptionalPad, P2: OptionalPad> ReplacePad<Pad3>
+    for Pads<P0, P1, P2, NoneT>
+{
+    fn replace<I: IsPad<PadNum = Pad3>>(self, pin: I) -> impl IsPads {
+        Pads(self.0, self.1, self.2, pin)
+    }
+}
+
+//==============================================================================
 // IoSet
 //==============================================================================
 
